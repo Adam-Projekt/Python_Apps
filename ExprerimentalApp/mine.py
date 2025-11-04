b9521c308c44f9779560e62a52c300cf235a4105
Adam-Projekt <adammegarules1@gmail.com>
2025-11-04 09:20:27 +0100

    update

--- ExprerimentalApp/mine.py
+++ ExprerimentalApp/mine.py
@@ -3,26 +3,52 @@ import time
 import random
 import keyboard
 
+grid = [
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+            ]
+
 def lose(): p.click(1000,300)
 def checkwin(): return not p.pixelMatchesColor(1026, 293, (255, 255, 0))
 def checklose(): return not p.pixelMatchesColor(1022,311,(255,255,0)) 
 def checkalive(): return not checklose() or checkwin()
-def clickrandom(): 
-    p.click(random.randint(1,9)*50 + 800,random.randint(1,9)*50 + 400) 
-    if random.randint(1,4) == 1: p.rightClick(random.randint(1,9)*50 + 800,random.randint(1,9)*50 + 400) 
+def clickTile(x,y): 
+    p.click(x*50 + 800,y*50 + 400) 
+def clickMine(x,y): 
+    p.click(x*50 + 800,y*50 + 400,button='right')
+    
 print("Jdi do minespeewer do 3 sekund")
-
 time.sleep(3)
 active = True
 while active:
+    print(grid)
     if keyboard.is_pressed('e'):
         active = False
     if checklose(): 
+        grid = [
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1,
+            -1,-1,-1,-1,-1,-1,-1,-1,-1
+        ]
         lose()
     elif checkwin(): 
         active = False
     elif checkalive():
-        clickrandom() 
+        clickTile(4,0)
+        clickTile(4,1)
     
 print(checkwin())
 print(checklose())
